3c78fcc1934c2f0c71d5989ab23ad65b986cdd3e
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2026-02-11 02:08:26 +0530

    [Ashutosh] feat: added get emolpyee method

--- src/main/java/com/example/employeepayroll/service/EmployeeService.java
+++ src/main/java/com/example/employeepayroll/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.example.employeepayroll.service;
 
+import com.example.employeepayroll.exception.ResourceNotFoundException;
 import com.example.employeepayroll.model.Employee;
 import com.example.employeepayroll.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,5 +15,11 @@ public class EmployeeService {
 
     public List<Employee> getAll() {
         return repo.findAll();
+    }
+
+    public Employee getById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee not found: " + id));
     }
 }
