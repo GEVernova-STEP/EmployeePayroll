e34c02baeb238d9fb8ad73c2896721b9c337b4a5
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2026-02-11 01:58:02 +0530

    [Ashutosh] feat: add constraint to the employee model

--- src/main/java/com/example/employeepayroll/model/Employee.java
+++ src/main/java/com/example/employeepayroll/model/Employee.java
@@ -1,6 +1,10 @@
 package com.example.employeepayroll.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -16,9 +20,13 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be 2–50 characters")
     @Column(nullable = false)
     private String name;
 
+    @NotNull(message = "Salary required")
+    @Positive(message = "Salary must be positive")
     @Column(nullable = false)
     private Double salary;
 }
