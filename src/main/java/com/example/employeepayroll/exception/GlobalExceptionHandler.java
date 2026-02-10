8091ddd4c47166158ad41f236b163f58502251bc
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2026-02-11 02:19:11 +0530

    [Ashutosh] feat: added global exception handler

---
+++ src/main/java/com/example/employeepayroll/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.example.employeepayroll.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFound(ResourceNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidation(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+                .forEach(err ->
+                        errors.put(err.getField(), err.getDefaultMessage()));
+
+        return errors;
+    }
+}
