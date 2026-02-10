c37ac8334a79a98147f6b50b42377946ee09f69f
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2026-02-11 01:48:47 +0530

    [Ashutosh] feat: added employee model

---
+++ src/main/java/com/example/employeepayroll/model/Employee.java
@@ -0,0 +1,24 @@
+package com.example.employeepayroll.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double salary;
+}
